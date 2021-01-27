244946df3e8a7c18ddc7122bb43df8edb6ab086d
SpideyStan581994 <steven.tan@ulaps.dev>
2021-01-27 17:19:08 +0800

    Added password Encoder.

--- src/main/java/com/stan/demo/service/AuthService.java
+++ src/main/java/com/stan/demo/service/AuthService.java
@@ -4,6 +4,7 @@ import com.stan.demo.dto.RegisterRequest;
 import com.stan.demo.model.User;
 import com.stan.demo.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 
@@ -12,17 +13,23 @@ public class AuthService {
 
 
     private final UserRepository  userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public AuthService(UserRepository userRepository) {
+    public AuthService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public void signup(RegisterRequest registerRequest) {
         User user = new User();
         user.setUserName(registerRequest.getUsername());
-        user.setPassword(registerRequest.getPassword());
+        user.setPassword(encodePassword(registerRequest.getPassword()));
         user.setEmail(registerRequest.getEmail());
         userRepository.save(user);
+    }
+
+    private String encodePassword(String password) {
+        return passwordEncoder.encode(password);
     }
 }
