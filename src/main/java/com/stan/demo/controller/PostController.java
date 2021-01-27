f41107e36b7c9b6f553656dea743bb25c7316d79
SpideyStan581994 <steven.tan@ulaps.dev>
2021-01-28 00:39:08 +0800

    Added PosDto.

--- src/main/java/com/stan/demo/controller/PostController.java
+++ src/main/java/com/stan/demo/controller/PostController.java
@@ -1,7 +1,17 @@
 package com.stan.demo.controller;
 
+import com.stan.demo.dto.PostDto;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/posts")
 public class PostController {
+
+    @PostMapping
+    public void createPost(@RequestBody PostDto postDto){
+
+    }
 }
